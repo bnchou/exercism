359c67b46c6d41bdbee51ec6f0379791937394f6
Bastien Chou <bchou@genetec.com>
2021-05-01 23:28:50 +0000

    Start ETL resolution

--- clojure/etl/src/etl.clj
+++ clojure/etl/src/etl.clj
@@ -1,5 +1,9 @@
 (ns etl)
+(require '[clojure.string :as str])
 
 (defn transform [source] ;; <- arglist goes here
   ;; your code goes here
-)
+(map (fn [[key value]] (merge (map (fn [word] (hash-map (str/lower-case word) key)) value))) source)
+  )
+
+(transform {1 ["APPLE" "ARTICHOKE"], 2 ["BOAT" "BALLERINA"]})
