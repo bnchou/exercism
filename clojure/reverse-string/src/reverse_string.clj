6e8c63f855ddb431e5856045ffcbb8d7cde84e12
Bastien Chou <bchou@genetec.com>
2021-05-03 08:30:41 +0000

    Fix mentor comment

--- clojure/reverse-string/src/reverse_string.clj
+++ clojure/reverse-string/src/reverse_string.clj
@@ -1,6 +1,5 @@
 (ns reverse-string)
 
-(defn reverse-string [s] ;; <- arglist goes here
-  ;; your code goes here
-  (apply str (for [x (range (dec (count s)) -1 -1)] (get s x)))
+(defn reverse-string [s]
+  (into [] (for [x (range (dec (count s)) -1 -1)] (get s x)))
 )
