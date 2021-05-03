2a301b6c025837f7e6dc7b2a1e3ab1abfe81f906
Bastien Chou <bchou@genetec.com>
2021-05-03 19:35:20 +0000

    Solve protein translation

--- clojure/protein-translation/src/protein_translation.clj
+++ clojure/protein-translation/src/protein_translation.clj
@@ -1,9 +1,20 @@
 (ns protein-translation)
 
-(defn translate-codon [] ;; <- arglist goes here
-      ;; your code goes here
-)
+(defn translate-codon [c]
+  (case c
+    "AUG" "Methionine"
+    ("UUU", "UUC") "Phenylalanine"
+    ("UUA", "UUG") "Leucine"
+    ("UCU", "UCC", "UCA", "UCG") "Serine"
+    ("UAU", "UAC") "Tyrosine"
+    ("UGU", "UGC") "Cysteine"
+    "UGG" "Tryptophan"
+    "Unrecognized"))
 
-(defn translate-rna [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn divide-rna
+  ([rna] (divide-rna rna []))
+  ([rna codons]
+   (cond
+     (= rna "") codons
+     (some (fn [a] (= a (subs rna 0 3))) '("UAA" "UAG" "UGA")) codons
+     :else (divide-rna (subs rna 3 (count rna)) (conj codons (translate-codon (subs rna 0 3)))))))
