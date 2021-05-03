8997fc06c33d8b658f24a6270a5f87d825fa681d
Bastien Chou <bchou@genetec.com>
2021-05-03 21:15:31 +0000

    Remove comments

--- clojure/etl/src/etl.clj
+++ clojure/etl/src/etl.clj
@@ -1,6 +1,5 @@
 (ns etl)
 (require '[clojure.string :as str])
 
-(defn transform [source] ;; <- arglist goes here
-  ;; your code goes here
+(defn transform [source]
   (into {} (for [[k v] source] (into {} (for [w v] {(str/lower-case w) k})))))
