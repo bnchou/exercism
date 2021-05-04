58e51e919081d47a87676fbacf9e2614a64e3281
Bastien Chou <bchou@genetec.com>
2021-05-04 22:05:18 +0000

    Enhance slices

--- clojure/series/src/series.clj
+++ clojure/series/src/series.clj
@@ -1,9 +1,7 @@
 (ns series)
 
-(defn slices
-  ([string length] (slices string length []))
-  ([string length vector] (case length
-    0 [""]
-    (if (< (count string) length)
-      vector
-      (slices (subs string 1 (count string)) length (conj vector (subs string 0 length)))))))
+(defn slices [string length]
+  (if (= length 0)
+    [""]
+    (map (partial apply str) 
+         (partition length 1 string))))
